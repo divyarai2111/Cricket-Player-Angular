685ede172a9c11b86a700fd83ab375375ff0add1
Divya Raikar <divya.raikar@ibm.com>
2021-03-30 08:15:39 +0530

    fav service

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -12,19 +12,8 @@ export class CourseDialogComponent implements OnInit {
   description:any;
   constructor( private dialogRef: MatDialogRef<CourseDialogComponent>,
     @Inject(MAT_DIALOG_DATA) data:any) { 
-     
-      console.log(data)
-      try{
-        if(data.event!=undefined)
       this.description = data.event;
-
-      }catch(e){
-        if(data.res!=undefined)
-        this.description = data.res;
-      }
-      
-      
-     
+      // if(data.event!=null)
       console.log("In description")
       console.log(this.description)
      
--- src/app/favorite-players/favorite-players.component.ts
+++ src/app/favorite-players/favorite-players.component.ts
@@ -12,62 +12,67 @@ import { PlayerService } from '../Services/player.service';
 })
 export class FavoritePlayersComponent implements OnInit {
 
-  players:any=[]
-  user:any=''
+  players: any = []
+  user: any = ''
   ngOnInit(): void {
-    this.user=localStorage.getItem("username")
+    this.user = localStorage.getItem("username")
     // console.log(localStorage.getItem("username"))
-    if(localStorage.getItem("username")==null){
-     
+    if (localStorage.getItem("username") == null) {
+
       this.router.navigate(["/login"]);
     }
     this.getFavPlayersList(localStorage.getItem("username"));
-    
+
   }
 
-  ngOnChanges(){
+  ngOnChanges() {
     this.getFavPlayersList(localStorage.getItem("username"))
   }
 
-  constructor(private favService:FavServiceService,private router:Router, public dialog: MatDialog,
-    private playerService:PlayerService){
+  constructor(private favService: FavServiceService, private router: Router, 
+    public dialog: MatDialog,
+    private playerService: PlayerService) {
 
   }
 
 
-  getFavPlayersList(username:any){
+  getFavPlayersList(username: any) {
     // let user=localStorage.getItem("username");
     // console.log(username)
-    this.favService.getAllFavplayers(username).subscribe((res:any)=>{
+    this.favService.getAllFavplayers(username).subscribe((res: any) => {
       this.players.push(res)
-     
+
     })
     // console.log(this.players[0])
   }
 
+ 
 
-  openDialog(event: any) {
-  
+  openDialog(val: any) {
+
     // console.log(event)
     const dialogConfig2 = new MatDialogConfig();
-  
-    this.playerService.getInitPlayersStats(event.pid).subscribe((res)=>{
-      // console.log("response")
-      // console.log(res)
-      dialogConfig2.data = {
-        res
-     };
-    })
-   
-    console.log(dialogConfig2)
-  
-  const dialogRef = this.dialog.open(CourseDialogComponent,dialogConfig2);
 
-  
+    this.playerService.getInitPlayersStats(val.pid).subscribe((event) => {
+      // console.log("response")
+     let response=event;
+     dialogConfig2.data = {
+      event
+    };
+    const dialogRef = this.dialog.open(CourseDialogComponent, dialogConfig2);
+
+     
+    })
+
+
+    // console.log(dialogConfig2.data.res)
+
+   
+
   }
 
-  delete(event:any){
-    this.favService.delete(event,this.user).subscribe((res:any)=>{
+  delete(event: any) {
+    this.favService.delete(event, this.user).subscribe((res: any) => {
       this.getFavPlayersList(this.user);
     })
   }
